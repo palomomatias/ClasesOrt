1884307af792abb0210cdf6c6a70d7d051a068a6
PalomoMatias <palomomatias@hotmail.com>
2019-08-28 01:54:02 -0300

    asdsd

--- pruebas/src/tests/test1.java
+++ pruebas/src/tests/test1.java
@@ -1,5 +1,12 @@
 package tests;
-
+import java.util.Scanner;
 public class test1 {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+
+			
+		input.close();
+	}
 
 }
